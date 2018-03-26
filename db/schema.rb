ed8e58283a07362afd1d17ce0d32a6ed2a017215
ViditChitkara <chitkara.vidit@gmail.com>
2018-03-26 21:21:23 +0530

    added name to shops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325103036) do
+ActiveRecord::Schema.define(version: 20180326155032) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20180325103036) do
     t.float    "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "specifications", force: :cascade do |t|
