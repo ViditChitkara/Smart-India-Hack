e430ba7463e3c8dadaecbd80daa5df46f50853f2
siddhantbajaj <sbajaj1997@gmail.com>
2018-03-29 12:01:27 +0530

    Added data dump

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328152232) do
+ActiveRecord::Schema.define(version: 20180329052119) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(version: 20180328152232) do
     t.datetime "updated_at",   null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "email"
   end
 
 end
